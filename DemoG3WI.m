617342c4f0ed9721c1fdb06aa9986fb53ba564c1
AGuRu123 <53339725+AGuRu123@users.noreply.github.com>
2023-08-28 21:12:31 +0800

    Update DemoG3WI.m

--- DemoG3WI.m
+++ DemoG3WI.m
@@ -13,6 +13,6 @@ ResultG3WI = [];
 k = 1;%number of selected features
 f=rank_G3WI(1:k);
 [Prior,PriorN,Cond,CondN]=MLKNN_train(train_data(:,f),train_target,Num,Smooth);
-[Outputs,Pre_Labels]=MLKNN_test1(train_data(:,f),train_target,test_data(:,f),test_target,Num,Prior,PriorN,Cond,CondN); 
+[Outputs,Pre_Labels]=MLKNN_test(train_data(:,f),train_target,test_data(:,f),test_target,Num,Prior,PriorN,Cond,CondN); 
 
 ResultG3WI  = EvaluationAll(Pre_Labels,Outputs,test_target);
