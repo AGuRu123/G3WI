7e2cdcd02e9394b3c04e08330eba93d73ec4bd3a
AGuRu123 <53339725+AGuRu123@users.noreply.github.com>
2023-08-28 20:50:51 +0800

    Update Initialization.m

--- function/Initialization.m
+++ function/Initialization.m
@@ -1,7 +1,7 @@
 function Apara = Initialization
 % Initialization for optimziation parameters
-    Apara.alpha = 2^-3; 
-    Apara.beta = 2^-1; 
+    Apara.alpha = 2^-1; 
+    Apara.beta = 2^1; 
     Apara.MaxIter = 20;
     Apara.tol = 1e-5;
 end
